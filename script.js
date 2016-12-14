983938ac488b0657ab32240edb5dd6be173130a1
Jason Goebel <Jason Goebel>
2016-12-13 19:58:05 -0600

    Slider Finished, Start Stop/Start Button

--- script.js
+++ script.js
@@ -1,18 +1,33 @@
-var time = 60 * 1; // in seconds;
-var transitionEffect = 'stroke-dasharray ' + time + 's linear';
+var time;
+var emptyBarColor = 'darkcyan';
 var clockFull = document.getElementById('clockFull');
 var clockEmpty = document.getElementById('clockEmpty');
-var emptyBarColor = 'darkcyan';
 var timerDisplay = document.getElementById('displayTimer');
+var slideBar = document.getElementById('slideBar');
+var slider = document.getElementById('slider');
+var innerCircle = document.getElementById('innerCircle');
+var startPos = 0;
+var sliderStartPosition = 0;
+var sliderCurPosition = 0;
 var timeRemaining;
 var timer;
 
 function start () {
+  var transitionEffect = 'stroke-dasharray ' + (time + 1) + 's linear'; // added 1 to offest
   clockFull.style.transition = transitionEffect;
   clockFull.style.strokeDasharray = '0 628';
 
   clockEmpty.style.transition = transitionEffect;
   clockEmpty.style.strokeDasharray = '628 628';
+
+  // When timer start lets hide the set timer scroll bar
+  slideBar.style.opacity = "0.3";
+  slideBar.addEventListener("transitionend", function(){
+    slider.removeEventListener('mousedown', startMove, false);
+  }, false);
+  slideBar.removeEventListener("transitionend", function(){}, false);
+
+  //slideBar.style.display = "none";
 
   // Updates timer every second
   timer = setInterval(tickTock, 1000);
@@ -50,8 +65,43 @@ function pause () {
 }
 
 
+// slider functionality
+function startMove () {
+  startPos = event.clientX;
+  sliderCurPosition = (event.clientX - startPos) + sliderStartPosition;
+  document.addEventListener('mousemove', moveSlider, false);
+}
+
+function stopMove () {
+  document.removeEventListener('mousemove', moveSlider, false);
+  sliderStartPosition = sliderCurPosition;
+  time = timeRemaining;
+}
+
+function moveSlider () {
+  if(sliderCurPosition >= 0 && sliderCurPosition <= 234){ // only move if in range
+    slider.style.left = sliderCurPosition;
+    sliderCurPosition = parseInt(((event.clientX - startPos) + sliderStartPosition) / 6 ) * 6 ;  // causes changes in increments of 6 allowing 40 min of time
+
+    if (sliderCurPosition < 0){ // stop from locking is range exceded
+      sliderCurPosition = 0;
+    } else if (sliderCurPosition > 234){  // stop from locking is range exceded
+      sliderCurPosition = 234;
+    }
+    timeRemaining = ((sliderCurPosition / 6)+1) * 60;
+    timeDisplay();
+  }
+}
+
+
 // Initialize is called at load and on reseting the timer
 function init () {
+  time = 60 * 20; // default to 20min
+
+  // event listeners
+  slider.addEventListener('mousedown', startMove, false);
+  document.addEventListener('mouseup', stopMove, false);
+
   clockFull.style.transition = "none";
   clockFull.style.strokeDasharray = '628 0';
   clockEmpty.style.transition = "none";
@@ -60,13 +110,23 @@ function init () {
   timeRemaining = time;
   clearInterval(timer);
   timeDisplay();
+
+  //Set slider position on load
+  sliderStartPosition = ((time/60) * 6) - 1 ;
+  slider.style.left = sliderStartPosition;
 }
 
-//clock[1].addEventListener("transitionend", function(){
-//  alert("complete");
-//}, false);
+// Actions to take when time is up
+clockFull.addEventListener("transitionend", function(){
+  innerCircle.style.transition = "1s";
+  innerCircle.style.backgroundColor = "darkred";
+  slideBar.style.opacity = "1";
+
+  init();
+}, false);
 
 init();
+
 
 
 
